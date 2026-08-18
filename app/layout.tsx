9a75054fc11a15831d6ff91297a070bf2df759e2
Abhilash Joga <abhilashjoga@gmail.com>
2026-08-17 21:28:28 -0600

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -29,8 +30,34 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-slate-950 text-white">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4LVD51SWCX"
+          strategy="afterInteractive"
+        />
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-4LVD51SWCX', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         {children}
 
+        {/* Vercel Analytics */}
         <Analytics />
       </body>
     </html>
